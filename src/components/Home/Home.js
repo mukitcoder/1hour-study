1bd3a5603830086717325eba690da72ae22a449e
mukitcoder <mukitbd71@gmail.com>
2022-03-05 10:23:47 +0600

    fix padding

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import Courses from '../Courses/Courses';
-import Expertise from '../Expertise/Expertise';
-import Hero from '../Hero/Hero';
-import Instructors from '../Instructors/Instructors';
-import Review from '../Review/Review';
-import SkillDevelopment from '../SkillDevelopment/SkillDevelopment';
-import StartCourse from '../StartCourse/StartCourse';
+import React from "react";
+import Courses from "../Courses/Courses";
+import Expertise from "../Expertise/Expertise";
+import Hero from "../Hero/Hero";
+import Instructors from "../Instructors/Instructors";
+import Review from "../Review/Review";
+import SkillDevelopment from "../SkillDevelopment/SkillDevelopment";
+import StartCourse from "../StartCourse/StartCourse";
 
 const Home = () => {
-    return (
-        <div>
-            <Hero/>
-            <Courses/>
-            <Expertise/>
-            <StartCourse/>
-            <Review/>
-            <SkillDevelopment/>
-            <Instructors/>
-     
-        </div>
-    );
+  return (
+    <div>
+      <Hero />
+      <Courses />
+      <Expertise />
+      <StartCourse />
+      <Review />
+      <SkillDevelopment />
+      <Instructors />
+    </div>
+  );
 };
 
 export default Home;
